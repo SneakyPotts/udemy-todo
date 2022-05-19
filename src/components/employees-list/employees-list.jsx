561bc7d30c87a4366d37328978f1be6a7ff4c2ba
Aleks G <57754379+SneakyPotts@users.noreply.github.com>
2022-05-19 15:01:34 +0300

    add method n event delete item;

--- src/components/employees-list/employees-list.jsx
+++ src/components/employees-list/employees-list.jsx
@@ -1,11 +1,15 @@
 import './employees-list.css';
 import EmployeesListItem from "../employees-list-item/employees-list-item";
 
-const EmployeesList = ({data}) => {
+const EmployeesList = ({data, onDelete}) => {
   return (
     <ul className='app-list list-group'>
       {data.map(({id, ...itemProps}) =>
-        <EmployeesListItem key={id} {...itemProps}/>
+        <EmployeesListItem
+          key={id}
+          {...itemProps}
+          onDelete={() => onDelete(id)}
+        />
       )}
     </ul>
   );
