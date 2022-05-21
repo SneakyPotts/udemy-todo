0f14fc25269cbab0361265f93fd42e2818f1bcff
Aleks G <57754379+SneakyPotts@users.noreply.github.com>
2022-05-21 14:40:09 +0300

    added checking empty name n salary field in addItem form;

--- src/components/employees-add-form/employees-add.form.jsx
+++ src/components/employees-add-form/employees-add.form.jsx
@@ -17,13 +17,14 @@ class EmployeesAddForm extends Component {
   }
   onSubmit = (e) => {
     e.preventDefault();
+    if (this.state.name.length < 3 || this.state.salary <= 0)
+      return;
 
     this.props.addItem(this.state.name, this.state.salary);
-
     this.setState({
       name: '',
       salary: ''
-    })
+    });
   }
 
   render() {
